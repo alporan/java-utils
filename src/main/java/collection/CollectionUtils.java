0936be2c01b9d55646f90ad5b324ca80372966e0
Alp <alporan@windowslive.com>
2021-10-24 22:17:12 +0300

    Implement removeDuplicates method for collections

--- src/main/java/collection/CollectionUtils.java
+++ src/main/java/collection/CollectionUtils.java
@@ -1,18 +1,24 @@
 package collection;
 
+import java.util.Collection;
+import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
-public abstract class CollectionUtils {
+public class CollectionUtils {
 
 	private CollectionUtils() {
 		throw new IllegalStateException("Utility class");
 	}
-	
+
 	public static <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
 		Set<Object> seen = ConcurrentHashMap.newKeySet();
 		return t -> seen.add(keyExtractor.apply(t));
+	}
+
+	public static <E> Set<E> removeDuplicates(Collection<E> collection) {
+		return new LinkedHashSet<>(collection);
 	}
 }
